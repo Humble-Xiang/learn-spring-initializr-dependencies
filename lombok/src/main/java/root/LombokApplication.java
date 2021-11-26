59d0c213bb66896b94169eb0558ae54b52aa700c
QingTianSmell <QingTianSmell@gmail.com>
2021-11-26 13:36:59 +0800

    build: add lombok module

---
+++ lombok/src/main/java/root/LombokApplication.java
@@ -0,0 +1,15 @@
+package root;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Humble.X
+ * @since 2021/11/26
+ */
+@SpringBootApplication
+public class LombokApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LombokApplication.class, args);
+    }
+}
